9068c8351f9bc99e7017d83a2d70c0c2152b4a10
Samir Maikap <maikap.samir@gmail.com>
2018-05-08 19:58:31 +0530

    Add relation with view

--- app/Mail/DeactivationMail.php
+++ app/Mail/DeactivationMail.php
@@ -10,15 +10,16 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class DeactivationMail extends Mailable
 {
     use Queueable, SerializesModels;
+    public $data;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
 
     /**
@@ -28,6 +29,6 @@ class DeactivationMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('email.deactivation');
     }
 }
