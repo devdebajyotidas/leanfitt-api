41c494392661636d9bc43b2b6be8afebe9a801e1
Samir Maikap <maikap.samir@gmail.com>
2018-05-11 00:50:31 +0530

    Authentication update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\PasswordResetLog;
 use App\Models\UserLog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -119,8 +120,6 @@ class UserController extends Controller
         return response()->json($response);
     }
 
-    /*Design database for password reste with a session token*/
-
     function recovery(Request $request){
         $response=new \stdClass();
         $email=$request->get('email');
@@ -130,19 +129,24 @@ class UserController extends Controller
             $user=User::where('mobile',$mobile)->first();
             $email=$user->email;
         }
-
-        if(empty($email)){
+        elseif(!empty($email) && empty($mobile)){
+            $user=User::where('email',$email)->first();
+        }
+        elseif(!empty($email) && !empty($mobile)){
+            $user=User::where('email',$email)->first();
+        }
+        else{
             $response->success=false;
             $response->message='Unable to find your account';
             return response()->json($response);
         }
 
-        $token=$email['token']=random_int(111111,999999);
-        $reset['token']=md5($token);
-        $reset['email']=$email;
-        $email['user_id']=isset($user->id) ? $user->id : '';
 
-        PasswordReset::create($reset);
+        $reset['request_id']=md5(time());
+        $reset['code']=$email['code']=random_int(111111,999999);
+        $reset['account_id']=isset($user->id) ? $user->id : '';
+
+        PasswordResetLog::create($reset);
 
         Mail::to($email)->send(new PasswordResetMail($email));
 
@@ -157,22 +161,75 @@ class UserController extends Controller
 
     }
 
+    function verifyEmailOtp(Request $request){
+        $response=new \stdClass();
+        $otp=$request->get('otp');
+        $request_id=$request->get('request_id');
+
+        if(empty($otp)){
+            $response->success=false;
+            $response->user_id=null;
+            $response->message="Please enter a valid otp";
+            return response()->json($response);
+        }
+
+        if(empty($request_id)){
+            $response->success=false;
+            $response->user_id=null;
+            $response->message="Invalid request id";
+            return response()->json($response);
+        }
+
+        $expiry=date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +30 minutes"));
+        $log=PasswordResetLog::where('request_id',$request_id)->where('otp',$otp)->where('created_at','<=',$expiry)->orderBy('created_at','desc')->first();
+        if($log){
+            $user=User::find($log->account_id);
+            if($user && $user->is_verified==0){
+                $user->is_verified=1;
+                $user->update();
+            }
+            $response->success=true;
+            $response->user_id=$log->account_id;
+            $response->message="Otp verified";
+        }
+        else{
+            $response->success=false;
+            $response->user_id=null;
+            $response->message="Otp not verified";
+        }
+
+        return response()->json($response);
+    }
+
     function resetPassword(Request $request){
          $response=new \stdClass();
-         $otp=$request->get('otp');
+         $user_id=$request->get('user_id');
 
-         if(empty($otp)){
+         if(empty($user_id)){
              $response->success=false;
-             $response->message="Please enter a valid otp";
+             $response->message="Unable to find the account";
              return response()->json($response);
          }
 
-//         $verified=PasswordReset::
+         $validator=Validator::make($request->all(),User::$rules['update']);
+         if($validator->passes()){
+             $user=User::find($user_id);
+             $user->password=$request->password;
+             if($user->update()){
+                 $response->success=true;
+                 $response->message="Password has been changed";
+             }
+             else{
+                 $response->success=false;
+                 $response->message="Unable to change the password";
+             }
 
+         }
+         else{
+             $response->success=false;
+             $response->message=$validator->errors()->first();
+         }
+
+        return response()->json($response);
     }
-
-    function verifyEmail(){
-
-    }
-
 }
