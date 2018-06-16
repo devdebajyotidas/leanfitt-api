d1fd7cec8a598b86c6ea1210a236348fec659eba
SamirM <maikap.samir@gmail.com>
2018-06-16 13:06:21 +0530

    Model update

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use App\Traits\UserAttributes;
 use Illuminate\Database\Eloquent\Model;
 
 class Award extends Model
 {
-    use UserAttributes;
     protected  $fillable=[
         'user_id',
         'title',
@@ -15,17 +13,7 @@ class Award extends Model
         'description'
     ];
 
-    protected $appends = [
-        'first_name',
-        'last_name',
-        'full_name',
-        'email',
-        'phone',
-        'avatar',
-    ];
-
-
     public function user(){
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 }
